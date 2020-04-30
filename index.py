d22f54502139a7b2dc4067db0cbbeba278679576
CyberHacky <suthardharmin@gmail.com>
2020-04-30 05:41:53 +0000

    Pentesting Modules added

---
+++ index.py
@@ -0,0 +1,277 @@
+#!/usr/bin/env python3
+
+import requests 
+import urllib.request
+import json                                # To use request package in current program 
+import sys
+import os
+import csv
+import webbrowser 
+from urllib.parse import urlparse
+import threading
+import time
+import socket, subprocess,sys
+import _thread
+import collections
+from datetime import datetime
+
+
+
+#Title print
+
+logo = ('''
+       ██████╗███████╗ █████╗ ███████╗██╗██████╗ ███╗  ███╗
+     ██╔════╝██╔════╝██╔══██╗██╔════╝██║██╔══██╗████╗ ████║
+     ╚█████╗ █████   ██║  ╚═╝█████╗  ██║██████╔╝██╔████╔██║
+      ╚═══██╗██╔══╝  ██║  ██╗██╔══╝  ██║██╔══██╗██║╚██╔╝██║
+     ██████╔╝███████╗╚█████╔╝██║     ██║██║  ██║██║ ╚═╝ ██║
+     ╚═════╝ ╚══════╝ ╚════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+[!] This Tool Must Run As ROOT [!]         Created By : SecFirm Team\n\n''')
+
+
+# Provide Custom Browser headers
+headers = {}
+headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'
+
+# Print Logo on startup
+print(logo)
+
+
+#Main Menu
+
+def mainmenu():
+    print ("""
+   {1}--Web Pentesting
+   {2}--Network Pentesting
+   {3}--Wireless Pentesting
+   {4}--Malware Analysis
+   {5}--DoS & DDoS
+   {6}--Security Hardening
+   {0}--Exit
+ """)
+    choice = input("SecFirm~# ")
+    if choice == "1":
+        webmain()
+    elif choice == "2":
+        networkmenu()
+    elif choice == "3":
+        wifimenu()
+    elif choice == "4":
+        malwaremenu()
+    elif choice == "5":
+        dosmenu()
+    elif choice == "6":
+        hardeningmenu()
+    elif choice == "0":
+        print("Thanks for Using SecFirm")
+        os.system('clear'), sys.exit()
+    elif choice == "":
+        print(logo)
+        print("\033[1m [+] Kindly Choose One Option \033[0m")
+        apimenu()
+    else:
+        print(logo)
+        mainmenu()
+
+# Run Netowrk Pentesting here
+
+def networkmenu():
+    os.system('clear')
+    print(logo)
+    print ("[+] Netowrk Pentesting Module : \n")
+
+    print ("""
+   {1}--Ping Sweep
+   {2}--TCP Scan
+   {0}--Back to Main Menu
+   {99}--Exit
+ """)
+    choice = input("SecFirm~# ")
+    if choice == "1":
+        pingsweepfun()
+    elif choice == "2":
+        tcpscanfun()
+    elif choice == "3":
+        wifimenu()
+    elif choice == "4":
+        malwaremenu()
+    elif choice == "5":
+        dosmenu()
+    elif choice == "6":
+        hardeningmenu()
+    elif choice == "0":
+        os.system('clear')
+        print(logo)
+        mainmenu()
+    elif choice == "99":
+        print("Thanks for Using SecFirm")
+        os.system('clear'), sys.exit()
+    elif choice == "":
+        print("\033[1m [+] Kindly Choose One Option \033[0m")
+        networkmenu()
+    else:
+        print(logo)
+        networkmenu()
+
+def pingsweepfun():
+
+    os.system('clear')
+    print(logo)
+    print ("[+] Netowrk Pentesting | Ping Sweep : \n")
+
+    ''' section 1 '''
+
+    net = input("Enter the Network Address ")
+    net1= net.split('.')
+    a = '.'
+    net2 = net1[0]+a+net1[1]+a+net1[2]+a
+    st1 = int(input("Enter the Starting Number "))
+    en1 = int(input("Enter the Last Number "))
+    en1 =en1+1
+    dic = collections.OrderedDict()
+    oper = platform.system()
+
+    if (oper=="Windows"):
+        ping1 = "ping -n 1 "
+    elif (oper== "Linux"):
+        ping1 = "ping -c 1 "
+    else :
+        ping1 = "ping -c 1 "
+    t1= datetime.now()
+
+    '''section 2'''
+    class myThread (threading.Thread):
+        def __init__(self,st,en):
+            threading.Thread.__init__(self)
+            self.st = st
+            self.en = en
+        def run(self):
+            run1(self.st,self.en)
+
+    '''section 3''' 
+    def run1(st1,en1):
+    #print "Scanning in Progess"
+        for ip in xrange(st1,en1):
+            #print ".",
+            addr = net2+str(ip)
+            comm = ping1+addr
+            response = os.popen(comm)
+        for line in response.readlines():
+            if(line.count("TTL")):
+                break
+            if (line.count("TTL")):
+            #print addr, "--> Live"
+                dic[ip]= addr
+
+    ''' Section 4  '''
+    total_ip =en1-st1
+    tn =20  # number of ip handled by one thread
+    total_thread = total_ip/tn
+    total_thread=total_thread+1
+    threads= []
+    try:
+        for i in xrange(total_thread):
+            en = st1+tn
+            if(en >en1):
+                en =en1
+                thread = myThread(st1,en)
+                thread.start()
+                threads.append(thread)
+                st1 =en
+    except:
+        print("Error: unable to start thread")
+        print("\t Number of Threads active:", threading.activeCount())
+
+
+    for t in threads:
+        t.join()
+        print ("Exiting Main Thread")
+        dict = collections.OrderedDict(sorted(dic.items()))
+
+    for key in dict:
+        print (dict[key],"-->" "Live")
+        t2= datetime.now()
+        total =t2-t1
+        print ("scanning complete in " , total)
+
+        networkmenu()
+
+
+def tcpscanfun():
+
+    os.system('clear')
+    print(logo)
+    print ("[+] Netowrk Pentesting | TCP Scan : \n")
+
+    '''section 1''' 
+    net = input("Enter the Network Address : ")
+    st1 = int(input("Enter the starting Number  : "))
+    en1 = int(input("Enter the last Number : "))
+    en1=en1+1
+    dic = collections.OrderedDict()
+    net1= net.split('.')
+    a = '.'
+    net2 = net1[0]+a+net1[1]+a+net1[2]+a
+    t1= datetime.now()
+
+    '''section 2'''
+    class myThread (threading.Thread):
+        def __init__(self,st,en):
+            threading.Thread.__init__(self)
+            self.st = st
+            self.en = en
+        def run(self):
+            run1(self.st,self.en)
+    
+    '''section 3'''
+    def scan(addr):
+        sock= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = sock.connect_ex((addr,135))
+        if result==0:
+            sock.close()
+            return 1
+        else :
+            sock.close()
+    
+    def run1(st1,en1):
+        for ip in xrange(st1,en1):
+            addr = net2+str(ip)
+        if scan(addr):
+            dic[ip]= addr
+
+    '''section 4'''
+    total_ip =en1-st1
+    tn =20  # number of ip handled by one thread
+    total_thread = total_ip/tn
+    total_thread=total_thread+1
+    threads= []
+    try:
+        for i in xrange(total_thread):
+            #print "i is ",i
+            en = st1+tn
+            if(en >en1):
+                en =en1
+                thread = myThread(st1,en)
+                thread.start()
+                threads.append(thread)
+                st1 =en
+    except:
+        print ("Error: unable to start thread")
+        print ("\t Number of Threads active:", threading.activeCount())
+
+        for t in threads:
+            t.join()
+            print ("Exiting Main Thread")
+            dict = collections.OrderedDict(sorted(dic.items()))
+
+        for key in dict:
+            print (dict[key],"-->" "Live")
+            t2= datetime.now()
+            total =t2-t1
+            print ("scanning complete in " , total)
+
+            networkmenu()
+# Call main menu to run program
+mainmenu()
