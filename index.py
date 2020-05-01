80d21764576a2fdaac6dd7f33eaf5c4d003010de
CyberHacky <suthardharmin@gmail.com>
2020-05-01 13:06:36 +0000

    Port Scanning Module added

--- index.py
+++ index.py
@@ -15,6 +15,7 @@ import _thread
 import collections
 from datetime import datetime
 import shelve
+from queue import Queue
 
 
 
@@ -116,6 +117,7 @@ def networkmenu():
         print(logo)
         networkmenu()
 
+# Ping SWEEP | Network Pentesting | Network Machine Detection
 def pingsweepfun():
 
     os.system('clear')
@@ -199,6 +201,7 @@ def pingsweepfun():
 
         networkmenu()
 
+# TCP Scan | Network Pentesting
 
 def tcpscanfun():
 
@@ -276,7 +279,52 @@ def tcpscanfun():
 
             networkmenu()
 
+# PORT SCANNING | Network Pentesting
 
+def portscanfun():
+
+    os.system('clear')
+    print(logo)
+    print ("[+] Netowrk Pentesting | Port Scan : \n")
+    
+    socket.setdefaulttimeout(0.25)
+    print_lock = threading.Lock()
+
+    target = input('Enter the host to be scanned: ')
+    t_IP = socket.gethostbyname(target)
+    print ('Starting scan on host: ', t_IP)
+
+    def portscan(port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            con = s.connect((t_IP, port))
+            with print_lock:
+                print(port, 'is open')
+            con.close()
+        except:
+            pass
+
+    def threader():
+        while True:
+            worker = q.get()
+            portscan(worker)
+            q.task_done()
+    
+    q = Queue()
+    startTime = time.time()
+   
+    for x in range(100):
+        t = threading.Thread(target = threader)
+        t.daemon = True
+        t.start()
+   
+    for worker in range(1, 500):
+        q.put(worker)
+    
+    q.join()
+    print('Time taken:', time.time() - startTime)
+
+    networkmenu()
 
 # Call main menu to run program
 mainmenu()
